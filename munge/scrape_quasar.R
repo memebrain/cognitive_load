135d0375eb186428ccace78cd15c1bcbae84cdb5
Alistair Walsh <divide5@gmail.com>
2013-02-14 11:42:40 +1100

    added columns to cleanQuasar named columns

--- munge/scrape_quasar.R
+++ munge/scrape_quasar.R
@@ -4,7 +4,9 @@ interesting<-paste0('./data/',allfiles[3:5])
 
 rawdf<-read.xls(file.choose(), pattern='Classification File',sheet=1, verbose=FALSE, blank.lines.skip=TRUE,header=TRUE,as.is=TRUE)
 
-cleanQuasar<-rawdf[2:7,c(1,6,10)]
+cleanQuasar<-rawdf[2:7,c(1,6,7,10,11)]
+
+names(cleanQuasar)<-c('file','AverageLinearEngagement', 'St.Dev.LinearEngagement', 'AverageLinearWorkload',  'St.Dev.LinearWorkload')
 
 lines<-sapply(interesting,function(x) readLines(x,3))
 
